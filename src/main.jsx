450115828c140d89c59ab9af6992c0ea198985aa
ahmed <ahmed@gmail.com>
2024-10-04 23:42:03 -0700

    backend data

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AppProvider } from './Appcontext/context.jsx'
-// import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
-    <HashRouter basename="/">
       <AppProvider>
         <App />
       </AppProvider>
-    </HashRouter>
-    {/* </BrowserRouter> */}
+  
   </React.StrictMode>
 )
+
+
+
+
+// how to render deploy  json server in github 
